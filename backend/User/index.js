97c8a564b3804e1eb23430522e8879eba08ee7b4
Satyajeetm715 <satyajeetmishra715@gmail.com>
2024-09-19 17:01:11 +0530

    i

---
+++ backend/User/index.js
@@ -0,0 +1,13 @@
+const mySum = ()=>{
+  console.log(2+2)
+  console.log("PREM")
+  console.log("AVS")
+  console.log("HARSH")
+    console.log("HARSH")
+    let a = 10;
+    console.log(a);
+    console.log("hii")
+  return 2+2;
+}
+
+mySum();
